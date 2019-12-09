26f7563cb06ff332f16ee52772a645858c3f182b
Will Coan <wcoan300@gmail.com>
2019-12-09 17:28:24 -0500

    added calender hours and a todo text box

---
+++ script/script.js
@@ -0,0 +1,86 @@
+
+
+$(".showDate").text(moment().format("MMM Do"))
+
+
+
+$(document).ready(function(){
+    $("#save1").click(function(){
+      $("#modal-container1 ").hide();
+    });
+    $("#am9").click(function(){
+      $("#modal-container1 ").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save2").click(function(){
+      $("#modal-container2").hide();
+    });
+    $("#am10").click(function(){
+      $("#modal-container2").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save3").click(function(){
+      $("#modal-container3").hide();
+    });
+    $("#am11").click(function(){
+      $("#modal-container3").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save4").click(function(){
+      $("#modal-container4").hide();
+    });
+    $("#pm12").click(function(){
+      $("#modal-container4").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save5").click(function(){
+      $("#modal-container5").hide();
+    });
+    $("#pm1").click(function(){
+      $("#modal-container5").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save6").click(function(){
+      $("#modal-container6").hide();
+    });
+    $("#pm2").click(function(){
+      $("#modal-container6").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save7").click(function(){
+      $("#modal-container7").hide();
+    });
+    $("#pm3").click(function(){
+      $("#modal-container7").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save8").click(function(){
+      $("#modal-container8").hide();
+    });
+    $("#pm4").click(function(){
+      $("#modal-container8").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#save9").click(function(){
+      $("#modal-container9").hide();
+    });
+    $("#pm5").click(function(){
+      $("#modal-container9").show();
+    });
+  });
