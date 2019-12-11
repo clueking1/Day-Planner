6545738c32e0933fb19a112b9f59cefebd57bb43
Will Coan <wcoan300@gmail.com>
2019-12-11 18:08:41 -0500

    changed stylings

--- script/script.js
+++ script/script.js
@@ -44,12 +44,16 @@ $(document).ready(function(){
     $(document).ready(function(){
         if (moment().format('HH') == 09) {
             $(".displayTodo").text($("#description1").val())
+           $("#body1").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 09) {
             $("#am9").css("width", "250px")
             $("#am9").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body1").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#am9").css("width", "250px")
             $("#am9").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body1").css("background-color", "rgba(7, 51, 66, 0.4)")
+            
         }
     })
 
@@ -57,12 +61,15 @@ $(document).ready(function(){
         if (moment().format('HH') == 10) {
             $(".displayTodo").text($("#description2").val())
             $("#am10").css("width", "300px")
+            $("#body2").css("background-color", "rgba(20, 145, 185, 0.37)")
         }else if (moment().format('HH') < 10) {
             $("#am10").css("width", "250px")
             $("#am10").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body2").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#am10").css("background-color", "rgba(7, 51, 66, 0.4)")
             $("#am10").css("width", "250px")
+            $("#body2").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
         
     })
@@ -71,12 +78,15 @@ $(document).ready(function(){
         if (moment().format('HH') == 11) {
             $(".displayTodo").text($("#description3").val())
             $("#am11").css("width", "300px")
+            $("#body3").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 11) {
             $("#am11").css("width", "250px")
             $("#am11").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body3").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#am11").css("width", "250px")
             $("#am11").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body3").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -84,14 +94,17 @@ $(document).ready(function(){
         if (moment().format('HH') == 12) {
             $(".displayTodo").text($("#description4").val())
             $("#pm12").css("width", "300px")
+            $("#body4").css("background-color", "rgba(20, 145, 185, 0.37)")
             
             
         } else if (moment().format('HH') < 12) {
             $("#pm12").css("width", "250px")
             $("#pm12").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body4").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#pm12").css("width", "250px")
             $("#pm12").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body4").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -99,12 +112,15 @@ $(document).ready(function(){
         if (moment().format('HH') == 13) {
             $(".displayTodo").text($("#description5").val())
             $("#pm1").css("width", "300px")
+            $("#body5").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 13) {
             $("#pm1").css("width", "250px")
             $("#pm1").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body5").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#pm1").css("width", "250px")
             $("#pm1").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body5").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -112,12 +128,15 @@ $(document).ready(function(){
         if (moment().format('HH') == 14) {
             $(".displayTodo").text($("#description6").val())
             $("#pm2").css("width", "300px")
+            $("#body6").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 14) {
             $("#pm2").css("width", "250px")
             $("#pm2").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body6").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#pm2").css("width", "250px")
             $("#pm2").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body6").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -125,12 +144,15 @@ $(document).ready(function(){
         if (moment().format('HH') == 15) {
             $(".displayTodo").text($("#description7").val())
             $("#pm3").css("width", "300px")
+            $("#body7").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 15) {
             $("#pm3").css("width", "250px")
             $("#pm3").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body7").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#pm3").css("width", "250px")
             $("#pm3").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body7").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -138,12 +160,15 @@ $(document).ready(function(){
         if (moment().format('HH') == 16) {
             $(".displayTodo").text($("#description8").val())
             $("#pm4").css("width", "300px")
+            $("#body8").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 16) {
             $("#pm4").css("width", "250px")
             $("#pm4").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body8").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#pm4").css("width", "250px")
             $("#pm4").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body8").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -151,12 +176,17 @@ $(document).ready(function(){
         if (moment().format('HH') == 17) {
             $(".displayTodo").text($("#description9").val())
             $("#pm5").css("width", "300px")
+            $("#body9").css("background-color", "rgba(20, 145, 185, 0.37)")
         } else if (moment().format('HH') < 17) {
             $("#pm5").css("width", "250px")
             $("#pm5").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body9").css("background-color", "rgba(20, 145, 185, 0.2)")
+            $("#body9").css("background-color", "rgba(20, 145, 185, 0.2)")
         } else {
             $("#pm5").css("width", "250px")
             $("#pm5").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body9").css("background-color", "rgba(7, 51, 66, 0.4)")
+            $("#body9").css("background-color", "rgba(7, 51, 66, 0.4)")
         }
     })
 
@@ -166,6 +196,8 @@ $(document).ready(function(){
             $(".displayTodo").text("Go home and relax!!!!")
         } 
     })
+
+  
 
 //adds todos on the hours 9am-5pm
     $(document).ready(function(){
